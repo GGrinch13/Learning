5f40e2ff5259573b7e6e8f1487304623fb8905a1
GGrinch13 <giorgi.minashvili.1@btu.edu.ge>
2023-05-06 19:54:58 +0400

    updated

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import random
 import requests
 
 url = 'https://type.fit/api/quotes'
-input = input("Input author's name: ")
 if requests.get(url).status_code == 200:
-    print(f"{input} said: ")
     data = requests.get(url).json()
     rand_quote = random.choice(data)
-    print(rand_quote)
     print(f"{rand_quote['text']}")
+    print(f"{rand_quote['author']}")
 
 else:
     print("failed")
